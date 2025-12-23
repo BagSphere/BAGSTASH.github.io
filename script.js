9847131a57ce584de66025f6568a1b6ffc1c71ef
BagSphere <mdfaijanop@gmail.com>
2025-12-23 20:46:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,9 @@
-// Skeleton loader
-window.onload = () => {
-  setTimeout(() => {
-    document.getElementById("skeleton").style.display = "none";
-    document.getElementById("productGrid").classList.remove("hidden");
-  }, 1500);
-};
-
-// Product filter
-function filterProducts(type) {
-  const cards = document.querySelectorAll(".product-card");
-  cards.forEach(card => {
+function filter(type){
+  let cards = document.querySelectorAll('.product-card');
+  cards.forEach(card=>{
     card.style.display =
-      type === "all" || card.classList.contains(type)
-        ? "block"
-        : "none";
+      type === 'all' || card.classList.contains(type)
+      ? 'block'
+      : 'none';
   });
-}
-
-// PWA Install
-let deferredPrompt;
-window.addEventListener("beforeinstallprompt", e => {
-  e.preventDefault();
-  deferredPrompt = e;
-  document.getElementById("installPopup").style.display = "block";
-});
-
-document.getElementById("installBtn")?.addEventListener("click", () => {
-  deferredPrompt.prompt();
-});
-
-function closeInstall() {
-  document.getElementById("installPopup").style.display = "none";
 }
